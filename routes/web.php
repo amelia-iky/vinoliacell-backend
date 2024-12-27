adb2c90cb2031607e8d068a2c8ce7d26b58f17ec
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-12-28 00:27:52 +0800

    update routes use prefix /api

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->post('signup', 'AuthController@signup');
-$router->post('signin', 'AuthController@signin');
-
-$router->group(['middleware' => 'auth:api'], function () use ($router) {});
+$router->group(['prefix' => 'api'], function () use ($router) {
+    // Authentication
+    $router->post('signup', 'AuthController@signup');
+    $router->post('signin', 'AuthController@signin');
+});
