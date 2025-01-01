4b54a4b0f24eaf415c4935cc93d5cb055aa0568f
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2025-01-01 19:38:36 +0800

    define brand routes

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,7 @@ $router->group(['prefix' => 'api'], function () use ($router) {
     // Authentication
     $router->post('signup', 'AuthController@signup');
     $router->post('signin', 'AuthController@signin');
+
+    // Brands
+    $router->get('brands', 'BrandController@getAll');
 });
