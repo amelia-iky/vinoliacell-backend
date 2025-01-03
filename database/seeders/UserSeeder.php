4d8fb6fde536df5efa59c856cdde1f6aa2171de9
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2025-01-04 03:32:25 +0800

    add multiple user entries to UserSeeder with updated email and password

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,7 +15,16 @@ class UserSeeder extends Seeder
             'fullName' => 'Mochammad Syahrul Kurniawan',
             'address' => 'Jl. Raya Sudirman, Singaraja',
             'telp' => '083847847555',
-            'email' => 'user@email.com',
+            'email' => 'user1@email.com',
+            'password' => Hash::make('user123'),
+        ]);
+
+        User::create([
+            'userName' => 'Amelia',
+            'fullName' => 'Amelia Rizky Yuniar',
+            'address' => 'Jl. Raya Sudirman, Singaraja',
+            'telp' => '0881026241853',
+            'email' => 'user2@email.com',
             'password' => Hash::make('user123'),
         ]);
     }
