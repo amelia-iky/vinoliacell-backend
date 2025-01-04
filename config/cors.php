cec2bcc7f584c68c35b82f1af7796a87b6d869fb
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2025-01-04 16:47:22 +0800

    fix: increase CORS max_age to 86400 seconds

--- config/cors.php
+++ config/cors.php
@@ -6,6 +6,6 @@ return [
     'allowed_origins' => ['http://localhost:5173'],
     'allowed_headers' => ['Content-Type, Authorization'],
     'exposed_headers' => [],
-    'max_age' => 3600,
+    'max_age' => 86400,
     'supports_credentials' => false,
 ];
